e419e815ca4afee21fba34ea0c8683f6e9b42b92
EvgeniyKiselev <mr.kiseleff@list.ru>
2020-01-11 19:21:59 +0300

    text

--- src/BinaryNum.java
+++ src/BinaryNum.java
@@ -4,7 +4,7 @@ public class BinaryNum {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Input binary number: ");
+        System.out.println("Введите число в двоичной системе: ");
         String bin = sc.nextLine();
         int binNum = Integer.parseInt(bin, 2);
         System.out.println(binNum);
