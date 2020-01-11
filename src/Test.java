9d30500b13b0b76844e371c56472a8c67f749e97
EvgeniyKiselev <mr.kiseleff@list.ru>
2020-01-11 12:45:26 +0300

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("input name: ");
+        String name = in.nextLine();
+        System.out.println("age ? ");
+        int age = in.nextInt();
+        System.out.print("ves? ");
+        float ves = in.nextFloat();
+        System.out.printf("Name: %s \n Age = %d \n Height: %.2f \n", name, age, ves);
+
+    }
+}
