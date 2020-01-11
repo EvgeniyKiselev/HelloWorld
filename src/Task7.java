eb9e6e3ca2b84fab59dba0d3575a4684376f6e70
EvgeniyKiselev <mr.kiseleff@list.ru>
2020-01-12 00:59:15 +0300

    +Условие вывода данных

---
+++ src/Task7.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Task7 {
+    public static void main(String[] args) {
+        final int X = 7;
+        final int Y = 35;
+        final int Z = 4;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите с клавиатуры число, оно будет присвоено переменной number:");
+        int number = sc.nextInt();
+
+        if (number == X || number == Y || number == Z) {
+            System.out.println("Данное значение имеется в константах");
+        }
+        else {
+            System.out.println("Такой константы нет!");
+        }
+
+    }
+}
