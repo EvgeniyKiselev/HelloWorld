1f158072fd7e800eaf99b5aa054a4b830e5768f8
EvgeniyKiselev <mr.kiseleff@list.ru>
2020-01-12 22:07:33 +0300

    +Цикличный вывод данных на экран

---
+++ src/Task8.java
@@ -0,0 +1,9 @@
+public class Task8 {
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 50; i++) {
+            System.out.println(i);
+            System.out.print("\n");
+        }
+    }
+}
