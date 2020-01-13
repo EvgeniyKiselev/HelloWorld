f744b44fc530b33011239125a4c1947e6f03dae9
EvgeniyKiselev <mr.kiseleff@list.ru>
2020-01-13 12:40:17 +0300

    +даление всех пробелов в заданном тексте

--- src/Task12.java
+++ src/Task12.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
-public class Task11 {
+public class Task12 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите строку: ");
         String s = sc.nextLine();
-        System.out.print("Вы ввели строку: "+ s);
+        String ss = s.replaceAll(" ", "");
+        System.out.print("Введенная строка без пробелов: "+ ss);
 
     }
 }
