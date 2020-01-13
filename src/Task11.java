2b25ec78af143b0411bac3f37445d85094d3217a
EvgeniyKiselev <mr.kiseleff@list.ru>
2020-01-13 12:34:53 +0300

    +Ввод строки с клавиатуры

---
+++ src/Task11.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Task11 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String s = sc.nextLine();
+        System.out.print("Вы ввели строку: "+ s);
+       
+    }
+}
