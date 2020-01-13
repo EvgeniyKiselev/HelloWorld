bdbf59b044cdacecac99d238c4959325df523b5d
EvgeniyKiselev <mr.kiseleff@list.ru>
2020-01-13 13:43:24 +0300

    +Преобразование типов

---
+++ src/Task14.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Task14 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        String S = sc.nextLine();
+        int X = Integer. parseInt(S);
+        double Y = (double) X;
+
+        System.out.println("String: " + S + "\n" + "Int: " + X + "\n" + "Double: " + Y);
+
+
+    }
+}
--- src/Test1.java
+++ src/Test1.java
@@ -1,12 +1,21 @@
+import java.util.Random;
+
 public class Test1
 
 {
 
-    public static void main(String[] args)
+    public static void main(String args[])
 
     {
+        final Random random = new Random();
+        String result = "";
 
+        for(int i = 0; i < 10; i++){
+           // Random myRandom = null;
+            result += String.valueOf(random.nextInt()) + "\n";
+        }
 
+        System.out.println(result);
 
     }
 
