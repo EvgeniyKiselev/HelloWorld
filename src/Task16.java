d27ba0f10111ccb1b97353ce01680be8244c8216
EKiselev <mr.kiseleff@list.ru>
2020-01-14 17:07:03 +0300

    Запись данных в файл

--- src/Task16.java
+++ src/Task16.java
@@ -2,7 +2,7 @@ import java.io.*;
 //Выгрузка данных из файла на экран
 public class Task16 {
     public static void main(String[] args) {
-            String s;
+        String s;
         try (BufferedReader br = new BufferedReader
                 (new FileReader("C://Users//...//Desktop//notes.txt")))
         {
---
+++ src/Task17.java
@@ -0,0 +1,32 @@
+import java.io.*;
+//Запись данных в файл
+public class Task17 {
+    public static void main(String[] args) throws  IOException {
+        String iText;
+        String result = "";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите текст для записи в файл notes \nдля окончания записи введите слово 'стоп'");
+        FileWriter writer = new FileWriter("C://Users//...//Desktop//notes.txt");
+        while (true) {
+                System.out.print(": ");
+                iText = br.readLine();
+                if (iText.equals("стоп"))
+                 {
+                   break;
+                   }
+                 writer.write(iText + "\n");
+        }
+        br.close();
+        writer.close();
+        System.out.println("Текст записан в файл notes.txt");
+    }
+}
+
+
+
+
+
+
+
+
+
