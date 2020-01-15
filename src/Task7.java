024ceb0d7b482ddbb3395dcdd406579253c5daf9
EKiselev <mr.kiseleff@list.ru>
2020-01-15 16:42:20 +0300

    Редактирование Task7

--- src/Task7.java
+++ src/Task7.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
+
 //Условие вывода данных
+
 public class Task7 {
     public static void main(String[] args) {
         final int X = 7;
         final int Y = 35;
         final int Z = 4;
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите с клавиатуры число, оно будет присвоено переменной number:");
         int number = sc.nextInt();
+        
         if (number == X || number == Y || number == Z) {
             System.out.println("Данное значение имеется в константах");
         }
