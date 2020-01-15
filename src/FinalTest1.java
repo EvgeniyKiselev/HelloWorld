e1d29d348d42c2c68152c5d7d2bae1c91fb17b50
EKiselev <mr.kiseleff@list.ru>
2020-01-15 16:20:52 +0300

    Редактирование FinalTest1 относительно рекомендаций по оформлению

--- src/FinalTest1.java
+++ src/FinalTest1.java
@@ -1,14 +1,18 @@
 import java.io.*;
 import java.util.InputMismatchException;
-//Итоговая задача №1:
-//Ввести с консоли число в бинарном формате. Перевести его в десятичный формат, записать в переменную int и вывести на экран.
-// Необходимо использовать циклы, нельзя использования готовые методы языка Java, для перевода числа из одной системы счисления в другую.
+
+///Итоговая задача №1:
+///Ввести с консоли число в бинарном формате. Перевести его в десятичный формат, записать в переменную int и вывести на экран.
+/// Необходимо использовать циклы, нельзя использования готовые методы языка Java, для перевода числа из одной системы счисления в другую.
+
 public class FinalTest1<CheckString> {
     public static void main(String[] args) throws IOException {
         BufferedReader inputString = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите число в двоичной системе: ");
         String binaryNumStr = inputString.readLine();
-        //проверка корректности введенных данных
+
+        ///проверка корректности введенных данных
+
         boolean check = checkString(binaryNumStr);
         while (!check) {
             System.out.println("Некорректный ввод, введите число в двоичной системе: ");
@@ -17,34 +21,36 @@ public class FinalTest1<CheckString> {
             check = checkString(binaryNumStr);
         }
         inputString.close();
-        //перевод в двоичную систему
-        int n = 0;
+
+        ///перевод в десятичную систему
+
+        int doubleToDecimal = 0;
         try {
             for (int j = 0; j < binaryNumStr.length(); j++) {
-                n <<= 1;
-                if (binaryNumStr.charAt(j) == '1') n++;
+                doubleToDecimal <<= 1;
+                if (binaryNumStr.charAt(j) == '1') doubleToDecimal++;
             }
-            System.out.println(n);
+            System.out.println(binaryNumStr + " В десятичной системе = " + doubleToDecimal);
         } catch (InputMismatchException ex) {System.out.println("Это не число" + ex);
         } finally {
             inputString.close();
         }
     }
 
-    public static boolean checkString(String string) {
-        if (string == null || string.length() == 0) return false;
+    public static boolean checkString(String writeFromConsoleString) {
+        if (writeFromConsoleString == null || writeFromConsoleString.length() == 0) return false;
 
         int i = 0;
-        if (string.charAt(0) == '-') {
-            if (string.length() == 1) {
+        if (writeFromConsoleString.charAt(0) == '-') {
+            if (writeFromConsoleString.length() == 1) {
                 return false;
             }
             i = 1;
         }
 
         char c;
-        for (; i < string.length(); i++) {
-            c = string.charAt(i);
+        for (; i < writeFromConsoleString.length(); i++) {
+            c = writeFromConsoleString.charAt(i);
             if (!(c >= '0' && c <= '1')) {
                 return false;
             }
